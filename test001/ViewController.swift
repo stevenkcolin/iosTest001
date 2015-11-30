ef525ac815446240268f0eea8b8aaeb3cb42980d
chenlin <chenlin@gepingtech.com>
2015-11-30 08:18:34 +0800

    Initial Commit

---
+++ test001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test001
+//
+//  Created by Lin Chen on 11/30/15.
+//  Copyright (c) 2015 Lin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
